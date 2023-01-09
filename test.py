e69867c51dc32c57959f17dd4972af9424402b42
DAEHO0 <tigrix0316@naver.com>
2023-01-09 20:23:53 +0900

    TCO 입력 기능 추가

---
+++ test.py
@@ -0,0 +1,51 @@
+num = input('TCO는 몇 명입니까?:  ')
+
+if num == '1':
+    print(f'TCO는 {num}명 입니다.')
+    print(f'TCO {num}명으론 근무가 성립되지 않습니다.')
+    print('다시 입력해주세요.')
+    1
+
+elif num == '2':
+    print(f'TCO는 {num}명 입니다.')
+    print(f'TCO {num}명으론 근무가 성립되지 않습니다.')
+    print('다시 입력해주세요.')
+    
+elif num == '3':
+    print(f'TCO는 {num}명 입니다.')
+    print('TCO 이름을 입력해주세요.')
+    TCO1 = input('TCO1:  ')
+    print(f'TCO1은 {TCO1} 입니다.')
+    TCO2 = input('TCO2:  ')
+    print(f'TCO2은 {TCO2} 입니다.')
+    TCO3 = input('TCO3:  ')
+    print(f'TCO3은 {TCO3} 입니다.')
+    tco = [TCO1, TCO2, TCO3]
+    
+elif num == '4':
+    print(f'TCO는 {num}명 입니다.')
+    print('TCO 이름을 입력해주세요.')
+    TCO1 = input('TCO1:  ')
+    print(f'TCO1은 {TCO1} 입니다.')
+    TCO2 = input('TCO2:  ')
+    print(f'TCO2은 {TCO2} 입니다.')
+    TCO3 = input('TCO3:  ')
+    print(f'TCO3은 {TCO3} 입니다.')
+    TCO4 = input('TCO4:  ')
+    print(f'TCO4은 {TCO4} 입니다.')
+    tco = [TCO1, TCO2, TCO3, TCO4]
+    
+elif num == '5':
+    print(f'TCO는 {num}명 입니다.')
+    print('TCO 이름을 입력해주세요.')
+    TCO1 = input('TCO1:  ')
+    print(f'TCO1은 {TCO1} 입니다.')
+    TCO2 = input('TCO2:  ')
+    print(f'TCO2은 {TCO2} 입니다.')
+    TCO3 = input('TCO3:  ')
+    print(f'TCO3은 {TCO3} 입니다.')
+    TCO4 = input('TCO4:  ')
+    print(f'TCO4은 {TCO3} 입니다.')
+    TCO5 = input('TCO5:  ')
+    print(f'TCO5은 {TCO5} 입니다.')
+    tco = [TCO1, TCO2, TCO3, TCO4, TCO5]
--- 조원 랜덤 생성.py
+++ 조원 랜덤 생성.py
@@ -1,22 +1,11 @@
 import random
 
-tco = ['이원찬', '조성원', '정민서', '박선빈']
-tca = ['김동규', '박상준', '방지언', '박진호', '정광렬', '김대호', '한대인']
-com = ['한대인', '김대호', '정광렬', '박진호', '방지언', '박상준', '김동규']
+tco = []
+tca_com = []
 
 #   -> 근무자 랜덤 생성, 리스트 안 객체들을 무작위로 생성해내는 코드 필요(O)
 #      단, tca와 com 랜덤하게 생성하되 tca와 com은 같으면 안 됨(O)
 #      당일 근무자는 다음 날 근무를 설 수 없음(이틀 연속으로 근무 못 함)
-'''
-day = 0
-
-while day < 32: 
-    r_tco = random.choice(tco) # TCO 1명 랜덤 추출
-    r_tc = random.sample(tca_com, 2) # TCA, COM 2명 중복 제외 랜덤 추출
-#       -> 각 날마다 tco, tca_com 모두 달라야 함
-    print(day, '일', r_tco, r_tc)
-    day += 1
-'''
 
 n = input("해당 월을 입력해주세요\t:")
 if n == '1':
@@ -24,10 +13,9 @@ if n == '1':
         print('-' * 23, ' 1월 ', '-' * 23) # 해당 월 입력
         day = 1
         while day < 32: # 1월, 1 ~ 31일까지
-            r_tco = random.choice(tco) # TCO 1명 랜덤 추출
-            r_tca = random.choice(tca) # TCA 1명 랜덤 추출
-            r_com = random.choice(com) # COMMO 1명 랜덤 추출
-            print(f'{day}일\t: TCO = {r_tco}, TCA = {r_tca}, COMMO = {r_com}')
+            r_tco = random.choice(tco)  # TCO 1명 랜덤 추출
+            r_tc = random.sample(tca_com, 2)    # TCA, COM 2명 중복 제외 랜덤 추출
+            print(f'{day}일 : TCO = {r_tco}, TCA/COMMO = {r_tc}')
             print(f'근무퇴근 : {leave}')
             print(f'근무OFF  : {off}')
             day += 1
