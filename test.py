e811cd86cfa30a9dd4c7b09509c3b135b23a8166
DAEHO0 <tigrix0316@naver.com>
2023-01-09 20:26:00 +0900

    test 내용 수정

--- test.py
+++ test.py
@@ -8,8 +8,12 @@ if num == '1':
 
 elif num == '2':
     print(f'TCO는 {num}명 입니다.')
-    print(f'TCO {num}명으론 근무가 성립되지 않습니다.')
-    print('다시 입력해주세요.')
+    print('TCO 이름을 입력해주세요.')
+    TCO1 = input('TCO1:  ')
+    print(f'TCO1은 {TCO1} 입니다.')
+    TCO2 = input('TCO2:  ')
+    print(f'TCO2은 {TCO2} 입니다.')
+    tco = [TCO1, TCO2]
     
 elif num == '3':
     print(f'TCO는 {num}명 입니다.')
@@ -49,3 +53,20 @@ elif num == '5':
     TCO5 = input('TCO5:  ')
     print(f'TCO5은 {TCO5} 입니다.')
     tco = [TCO1, TCO2, TCO3, TCO4, TCO5]
+    
+elif num == '6':
+    print(f'TCO는 {num}명 입니다.')
+    print('TCO 이름을 입력해주세요.')
+    TCO1 = input('TCO1:  ')
+    print(f'TCO1은 {TCO1} 입니다.')
+    TCO2 = input('TCO2:  ')
+    print(f'TCO2은 {TCO2} 입니다.')
+    TCO3 = input('TCO3:  ')
+    print(f'TCO3은 {TCO3} 입니다.')
+    TCO4 = input('TCO4:  ')
+    print(f'TCO4은 {TCO3} 입니다.')
+    TCO5 = input('TCO5:  ')
+    print(f'TCO5은 {TCO5} 입니다.')
+    TCO6 = input('TCO6:  ')
+    print(f'TCO6은 {TCO6} 입니다.')
+    tco = [TCO1, TCO2, TCO3, TCO4, TCO5, TCO6]
